a7da2d3af41b10947ef0508515fbbc437c0a67d9
Eliot Howes <ejhowes@gmail.com>
2019-04-25 16:31:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = { artist: "song"};
+var playlist = { artistName: "song"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, {[artistName]: songTitle})
